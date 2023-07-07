c54af5d8742d71a395cedc897eab0eca379c4f93
DarthRL <darthrlu@gmail.com>
2023-07-07 17:51:30 +0800

    9.3

--- exercise-calculator/bmiCalculator.ts
+++ exercise-calculator/bmiCalculator.ts
@@ -1,31 +1,53 @@
+import { isNotNumber } from "./utils";
+
 const calculateBmi = (height: number, weight: number): string => {
-  const bmi = parseFloat((weight / (height / 100) ** 2).toFixed(1))
+  const bmi = parseFloat((weight / (height / 100) ** 2).toFixed(1));
   switch (true) {
     case bmi < 16:
-      return 'Underweight (Severe thinness)'
-      break
+      return "Underweight (Severe thinness)";
+      break;
     case bmi < 17:
-      return 'Underweight (Moderate thinness)'
-      break
+      return "Underweight (Moderate thinness)";
+      break;
     case bmi < 18.5:
-      return 'Underweight (Mild thinness)'
-      break
+      return "Underweight (Mild thinness)";
+      break;
     case bmi < 25:
-      return 'Normal (healthy weight)'
-      break
+      return "Normal (healthy weight)";
+      break;
     case bmi < 30:
-      return 'Overweight (Pre-obese)'
-      break
+      return "Overweight (Pre-obese)";
+      break;
     case bmi < 35:
-      return 'Obese (Class I)'
-      break
+      return "Obese (Class I)";
+      break;
     case bmi < 40:
-      return 'Obese (Class II)'
-      break
+      return "Obese (Class II)";
+      break;
     default:
-      return 'Obese (Class III)'
-      break
+      return "Obese (Class III)";
+      break;
   }
-}
+};
 
-console.log(calculateBmi(180, 74))
+const parseArguments = (args: String[]): number[] => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNotNumber && !isNotNumber) {
+    return [Number(args[2]), Number(args[3])];
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+try {
+  const [height, weight] = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Error: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
--- exercise-calculator/exerciseCalculator.ts
+++ exercise-calculator/exerciseCalculator.ts
@@ -1,32 +1,36 @@
+import { isNotNumber } from "./utils";
+
 interface Result {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: 1 | 2 | 3
-  ratingDescription: 'bad' | 'not too bad but could be better' | 'good'
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: 1 | 2 | 3;
+  ratingDescription: "bad" | "not too bad but could be better" | "good";
+  target: number;
+  average: number;
 }
 
 const calculateExercises = (hours: number[], target: number): Result => {
-  const periodLength = hours.length
-  const trainingDays = hours.filter(n => n !== 0).length
-  const average = hours.reduce((a, b) => a + b) / periodLength
-  const success = average >= target
-  let rating, ratingDescription
+  console.log(hours);
+  console.log(target);
+  const periodLength = hours.length;
+  const trainingDays = hours.filter((n) => n !== 0).length;
+  const average = hours.reduce((a, b) => a + b) / periodLength;
+  const success = average >= target;
+  let rating, ratingDescription;
   switch (true) {
     case average < target / 2:
-      rating = 1 as const
-      ratingDescription = 'bad' as const
-      break
+      rating = 1 as const;
+      ratingDescription = "bad" as const;
+      break;
     case average < target:
-      rating = 2 as const
-      ratingDescription = 'not too bad but could be better' as const
-      break
+      rating = 2 as const;
+      ratingDescription = "not too bad but could be better" as const;
+      break;
     default:
-      rating = 3 as const
-      ratingDescription = 'good' as const
-      break
+      rating = 3 as const;
+      ratingDescription = "good" as const;
+      break;
   }
   return {
     periodLength,
@@ -35,8 +39,29 @@ const calculateExercises = (hours: number[], target: number): Result => {
     rating,
     ratingDescription,
     target,
-    average
-  }
-}
+    average,
+  };
+};
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const parseExerciseArguments = (args: String[]): [number[], number] => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  const full = args.slice(2);
+  full.forEach((a) => {
+    if (isNotNumber(a)) {
+      throw new Error("Provided values were not numbers!");
+    }
+  });
+  return [full.slice(1).map((a) => Number(a)), Number(full[0])];
+};
+
+try {
+  const [hours, target] = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(hours, target));
+} catch (error: unknown) {
+  let errorMessage = "Error: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}

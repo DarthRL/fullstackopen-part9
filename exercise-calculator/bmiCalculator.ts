41d52118e8938bf2ca422617b209aa21a764c04b
DarthRL <darthrlu@gmail.com>
2023-07-07 18:24:30 +0800

    9.5

--- exercise-calculator/bmiCalculator.ts
+++ exercise-calculator/bmiCalculator.ts
@@ -1,43 +1,43 @@
-import { isNotNumber } from "./utils";
+import { isNotNumber } from './utils';
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = parseFloat((weight / (height / 100) ** 2).toFixed(1));
   switch (true) {
     case bmi < 16:
-      return "Underweight (Severe thinness)";
+      return 'Underweight (Severe thinness)';
       break;
     case bmi < 17:
-      return "Underweight (Moderate thinness)";
+      return 'Underweight (Moderate thinness)';
       break;
     case bmi < 18.5:
-      return "Underweight (Mild thinness)";
+      return 'Underweight (Mild thinness)';
       break;
     case bmi < 25:
-      return "Normal (healthy weight)";
+      return 'Normal (healthy weight)';
       break;
     case bmi < 30:
-      return "Overweight (Pre-obese)";
+      return 'Overweight (Pre-obese)';
       break;
     case bmi < 35:
-      return "Obese (Class I)";
+      return 'Obese (Class I)';
       break;
     case bmi < 40:
-      return "Obese (Class II)";
+      return 'Obese (Class II)';
       break;
     default:
-      return "Obese (Class III)";
+      return 'Obese (Class III)';
       break;
   }
 };
 
 const parseArguments = (args: String[]): number[] => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
 
   if (!isNotNumber && !isNotNumber) {
     return [Number(args[2]), Number(args[3])];
   } else {
-    throw new Error("Provided values were not numbers!");
+    throw new Error('Provided values were not numbers!');
   }
 };
 
@@ -45,7 +45,7 @@ try {
   const [height, weight] = parseArguments(process.argv);
   console.log(calculateBmi(height, weight));
 } catch (error: unknown) {
-  let errorMessage = "Error: ";
+  let errorMessage = 'Error: ';
   if (error instanceof Error) {
     errorMessage += error.message;
   }
--- exercise-calculator/index.ts
+++ exercise-calculator/index.ts
@@ -1,11 +1,29 @@
-import express from "express";
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+import { isNotNumber } from './utils';
 const app = express();
 
-app.get("/hello", (_req, res) => {
-  res.send("Hello Full Stack");
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  try {
+    const { height, weight } = req.query;
+    if (isNotNumber(height) || isNotNumber(weight)) {
+      return res.status(400).json({ error: 'malformatted parameters' });
+    }
+    return res.send({
+      weight,
+      height,
+      bmi: calculateBmi(Number(height), Number(weight)),
+    });
+  } catch (error) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

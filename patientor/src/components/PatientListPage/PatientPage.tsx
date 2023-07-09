02a332c5e3e836ab515cefb284fea4a7cbaafe57
DarthRL <darthrlu@gmail.com>
2023-07-09 22:27:29 +0800

    9.24

--- patientor/src/components/PatientListPage/PatientPage.tsx
+++ patientor/src/components/PatientListPage/PatientPage.tsx
@@ -1,15 +1,19 @@
-import { Patient } from '../../types';
+import { Diagnosis, Patient } from '../../types';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import patientService from '../../services/patients';
 
-const PatientPage = () => {
+interface Props {
+  diagnoses: Diagnosis[];
+}
+
+const PatientPage = ({ diagnoses }: Props) => {
   const [patient, setPatient] = useState<Patient | null>();
   const id = useParams().id;
   useEffect(() => {
-    patientService.getById(id as string).then(data => setPatient(data))
+    patientService.getById(id as string).then((data) => setPatient(data));
   }, [id]);
 
   if (!patient) return <div>Loading</div>;
@@ -31,6 +35,23 @@ const PatientPage = () => {
       </h2>
       <div>ssn: {patient.ssn}</div>
       <div>occupation: {patient.occupation}</div>
+      <h3>entries</h3>
+      {patient.entries.map((e) => (
+        <div key={e.id}>
+          <div>
+            {e.date} <i>{e.description}</i>
+          </div>
+          {!e.diagnosisCodes ? null : (
+            <ul>
+              {e.diagnosisCodes.map((code) => (
+                <li key={code}>
+                  {code} {diagnoses.find((d) => d.code === code)?.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
     </div>
   );
 };

1323de7c0e46019b88bf3c900dc2196e6c2b6568
DarthRL <darthrlu@gmail.com>
2023-07-09 17:05:20 +0800

    9.16

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,6 +1,27 @@
-const App = () => {
+import { useEffect, useState } from 'react';
+import { NonSensitiveDiaryEntry } from './types';
+import { getAllDiaries } from './services/diaryService';
 
-  return <div></div>;
+const App = () => {
+  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  useEffect(() => {
+    getAllDiaries().then(data => {
+      setDiaries(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Diary entries</h2>
+      {diaries.map((d, i) => (
+        <div key={i}>
+          <h3>{d.date}</h3>
+          <div>visibility: {d.visibility}</div>
+          <div>weather: {d.weather}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
